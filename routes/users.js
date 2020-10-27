acd41e94231e8028d310cf0c8196cf33a9a31e97
Ilya <iliakrapivkin@gmail.com>
2020-10-27 19:14:19 +0300

    +

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('GET from router USERS');
+  res.json({ name: 'Tomagavk' });
 });
 
 module.exports = router;
