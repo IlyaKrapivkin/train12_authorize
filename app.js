cb8ad150454304d25416355398124894e564deed
Ilya <iliakrapivkin@gmail.com>
2020-11-02 15:02:05 +0300

    cookie-counter

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-// CONSTS:
+// REQUIRE:
 const express = require('express');
 
-// const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+ 
 const app = express();
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 // MIDDLES:
 app.set('view engine', 'hbs');
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { title: 'Authorize' });
+});
+
+router.get('/testCK', (req, res) => {
+  let counter = Number(req.cookies.views);
+
+  if (!counter) {
+    counter = 0;
+  }
+
+  counter += 1;
+
+  res.cookie('views', counter, { httpOnly: false });
+  res.render('testCK', { counter });
 });
 
 module.exports = router;
